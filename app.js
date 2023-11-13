4a65ae0de3efb8cc606db024a420e9105fdb40b7
ISSAM EL IMRANY <90471805+issamelimrany@users.noreply.github.com>
2023-11-13 11:05:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -128,3 +128,26 @@ new Chart(userGraph, {
     }
   }
 });
+
+function updateValues(element, month) {
+
+        const desktopValue = Math.floor(Math.random() * 100);
+        const tabletValue = Math.floor(Math.random() * 100);
+        const mobileValue = Math.floor(Math.random() * 100);
+
+        document.getElementById('desktop-percent').textContent = desktopValue + '%';
+        document.getElementById('desktop-bar').style.width = desktopValue + '%';
+
+        document.getElementById('tablet-percent').textContent = tabletValue + '%';
+        document.getElementById('tablet-bar').style.width = tabletValue + '%';
+
+        document.getElementById('mobile-percent').textContent = mobileValue + '%';
+        document.getElementById('mobile-bar').style.width = mobileValue + '%';
+
+        const months = document.querySelectorAll('.select-month');
+        months.forEach(m => m.classList.remove('active'));
+
+        element.classList.add('active');
+
+
+      }
